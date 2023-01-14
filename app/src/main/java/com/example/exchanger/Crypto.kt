b152e639550c6c4705d5049a76b2dd49c9a8d1e1
luka karzhalovi <karzhalovi@gmail.com>
2023-01-14 16:05:06 +0400

    viewpager and adapter

---
+++ app/src/main/java/com/example/exchanger/Crypto.kt
@@ -0,0 +1,13 @@
+package com.example.exchanger
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class Crypto : Fragment(R.layout.fragment_crypto) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
